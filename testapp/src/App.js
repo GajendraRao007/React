bcf9363dca02627e0a2410ee96b09a545f19393d
GajendraRao007 <raogajendra27@gmail.com>
2024-03-27 17:30:40 +0530

    added comments

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from './components/header';
 import Body from './components/Body'
 import Footer from './components/footer'
-
+//-----------------------------------------------//
 function App() {
   return (
     <div className="App">
